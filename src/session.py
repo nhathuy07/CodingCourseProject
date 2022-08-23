1c116015bbef367d88cf37ce6f94fd72fb19d7d9
Huy Nguyen <nhathuy12@outlook.com>
2022-08-23 23:30:48 +0700

    Create Session Manager

--- src/session.py
+++ src/session.py
@@ -1,16 +1,57 @@
 
+from typing import Optional, Dict
+from common.types import Items
+import os
+
 class Session:
     def __init__(self):
         self.section = None
-        self.earnedItems = []
-    def load_savefile(self):
+        self.playerData: Dict[str, list] = {
+            "current_lvl" : None,
+            "earned_items": [],
+        }
+    def load_or_create_savefile(self):
         import json
-        import pathlib
         from common import paths
-        userSavefilePath = paths.DATA_PATH / "player_savefile.json"
-        saveFile = open(userSavefilePath, "r")
-        self.earnedItems = json.load(saveFile)["earned_items"]
+        self.userSavefilePath = paths.DATA_PATH / "player_savefile.json"
+        if os.path.exists(str(self.userSavefilePath)):
+            with open(self.userSavefilePath, "r") as saveFileIOWrapper:
+                self.playerData = json.load(saveFileIOWrapper)
+        else:
+            self.override_savefile()
 
+    def add_item(self, item: Items):
+        if self.playerData["earned_items"].count(item.name) == 0:
+            self.playerData["earned_items"].append(item.name)
+        
+    def update_savefile(self):
+        import json
+        from common import paths
+        self.userSavefilePath = paths.DATA_PATH / "player_savefile.json"
+        with open(self.userSavefilePath, "w") as saveFileIOWrapper:
+            saveFileIOWrapper.write(json.dumps(self.playerData))
+    
+    def override_savefile(self):
+        import json
+        from common import paths
+        self.userSavefilePath = paths.DATA_PATH / "player_savefile.json"
+        with open(self.userSavefilePath, "w") as saveFileIOWrapper:
+            self.playerData["earned_items"] = []
+            self.playerData["current_lvl"] = None
+            saveFileIOWrapper.write(json.dumps(self.playerData))
+
+    def update_section(self):
+        from common import types
+        if self.playerData['current_lvl'] == None:
+            self.section = types.GameSection.INTRO
+        elif 1 <= self.playerData['current_lvl'] <= 4:
+            self.section = types.GameSection.value(self.playerData['current_lvl'])
+    
+    def set_lvl(self, new_lvl):
+        self.playerData['current_lvl'] = new_lvl
+    def get_lvl(self):
+        return self.playerData['current_lvl']
 s = Session()
-s.load_savefile()
-print(s.earnedItems)
+s.update_savefile()
+
+print(s.playerData["earned_items"])
